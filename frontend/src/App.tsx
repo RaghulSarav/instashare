fac4845032465ed2ef7cd9e085f7d5510bf22c8b
RaghulSarav <raghulsaravanan1010@gmail.com>
2024-07-24 00:32:30 +0530

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
@@ -12,12 +12,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Signin />} />
           <Route path="/post/:id" element={<Blog />} />
           <Route path="/posts" element={<Blogs/>} />
           <Route path="/publish" element={<Publish/>}/>
         </Routes>
       </BrowserRouter>
+      
     </>
   )
 }
